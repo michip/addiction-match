ac0206d06cde4de33c59456190948c6352db2eda
upliftalter <34913266+upliftalter@users.noreply.github.com>
2020-11-07 23:25:19 +0100

    Cleaned up ChatWindow
    Risky push to try

--- frontend/src/views/ChatWindow.vue
+++ frontend/src/views/ChatWindow.vue
@@ -9,7 +9,7 @@
     <v-main class="grey lighten-5">
       <v-container class="d-flex justify-center">
         <Chat v-if="visible"
-              style="height: 500px; max-width: 800px"
+              style="height: 700px; max-width: 800px"
               :participants="participants"
               :myself="myself"
               :messages="messages"
@@ -18,7 +18,6 @@
               :colors="colors"
               :border-style="borderStyle"
               :hide-close-button="hideCloseButton"
-              :close-button-icon-size="closeButtonIconSize"
               :submit-icon-size="submitIconSize"
               :submit-image-icon-size="submitImageIconSize"
               :load-more-messages="toLoad.length > 0 ? loadMoreMessages : null"
@@ -28,10 +27,6 @@
               :send-images="false"
               :profile-picture-config="profilePictureConfig"
               :timestamp-config="timestampConfig"
-              :link-options="linkOptions"
-              :accept-image-types="'.png, .jpeg'"
-              @onImageClicked="onImageClicked"
-              @onImageSelected="onImageSelected"
               @onMessageSubmit="onMessageSubmit"
               @onType="onType"
               @onClose="onClose"/>
@@ -60,11 +55,6 @@ export default {
           id: 1,
           profilePicture: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/NafSadh_Profile.jpg/768px-NafSadh_Profile.jpg'
         },
-        {
-          name: 'José',
-          id: 2,
-          profilePicture: 'https://lh3.googleusercontent.com/-G1d4-a7d_TY/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJPez_wX5UCJztzEUeCxOd7HBK7-jA.CMID/s83-c/photo.jpg'
-        }
       ],
       myself: {
         name: 'Matheus S.',
@@ -73,29 +63,22 @@ export default {
       },
       messages: [
         {
-          content: 'received messages',
+          content: 'Hello there, can I talk to you?',
           myself: false,
           participantId: 1,
           timestamp: {year: 2019, month: 3, day: 5, hour: 20, minute: 10, second: 3, millisecond: 123},
           type: 'text'
         },
         {
-          content: 'sent messages',
+          content: 'Of course ! Feel free to talk to me',
           myself: true,
           participantId: 3,
           timestamp: {year: 2019, month: 4, day: 5, hour: 19, minute: 10, second: 3, millisecond: 123},
           type: 'text'
         },
-        {
-          content: 'other received messages',
-          myself: false,
-          participantId: 2,
-          timestamp: {year: 2019, month: 5, day: 5, hour: 10, minute: 10, second: 3, millisecond: 123},
-          type: 'text'
-        }
       ],
-      chatTitle: 'My chat title',
-      placeholder: 'send your message',
+      chatTitle: 'Discussing with my guardian angel',
+      placeholder: 'Say something',
       colors: {
         header: {
           bg: '#d30303',
@@ -104,11 +87,11 @@ export default {
         message: {
           myself: {
             bg: '#fff',
-            text: '#bdb8b8'
+            text: '#000000'
           },
           others: {
             bg: '#fb4141',
-            text: '#fff'
+            text: '#ffff'
           },
           messagesDisplay: {
             bg: '#f7f3f3'
@@ -123,7 +106,7 @@ export default {
         bottomLeft: "10px",
         bottomRight: "10px",
       },
-      hideCloseButton: false,
+      hideCloseButton: true,
       submitIconSize: 25,
       closeButtonIconSize: "20px",
       asyncMode: false,
@@ -149,7 +132,7 @@ export default {
       ],
       scrollBottom: {
         messageSent: true,
-        messageReceived: false
+        messageReceived: true
       },
       displayHeader: false,
       profilePictureConfig: {
@@ -165,44 +148,8 @@ export default {
         format: 'HH:mm',
         relative: false
       },
-      // there are other options, you can check them here
-      // https://soapbox.github.io/linkifyjs/docs/options.html
-      linkOptions: {
-        myself: {
-          className: 'myLinkClass',
-          events: {
-            click: function (e) {
-              alert('Link clicked!');
-            },
-            mouseover: function (e) {
-              alert('Link hovered!');
-            }
-          },
-          format: function (value, type) {
-            if (type === 'url' && value.length > 50) {
-              value = value.slice(0, 50) + '…';
-            }
-            return value;
-          }
-        },
-        others: {
-          className: 'othersLinkClass',
-          events: {
-            click: function (e) {
-              alert('Link clicked!');
-            },
-            mouseover: function (e) {
-              alert('Link hovered!');
-            }
-          },
-          format: function (value, type) {
-            if (type === 'url' && value.length > 50) {
-              value = value.slice(0, 50) + '…';
-            }
-            return value;
-          }
-        }
-      }
+
+
     }
   },
   methods: {
@@ -236,26 +183,7 @@ export default {
     onClose() {
       this.visible = false;
     },
-    onImageSelected(files, message) {
-      let src = 'https://149364066.v2.pressablecdn.com/wp-content/uploads/2017/03/vue.jpg'
-      this.messages.push(message);
-      /**
-       * This timeout simulates a requisition that uploads the image file to the server.
-       * It's up to you implement the request and deal with the response in order to
-       * update the message status and the message URL
-       */
-      setTimeout((res) => {
-        message.uploaded = true
-        message.src = res.src
-      }, 3000, {src});
-    },
-    onImageClicked(message) {
-      /**
-       * This is the callback function that is going to be executed when some image is clicked.
-       * You can add your code here to do whatever you need with the image clicked. A common situation is to display the image clicked in full screen.
-       */
-      console.log('Image clicked', message.src)
-    }
+
   }
 }
 </script>
