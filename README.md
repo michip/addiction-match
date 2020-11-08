9b0cfde30e75c6c1e47f78503df1776bfa812b44
Piotr Proszowski <p.proszowski@gmail.com>
2020-11-08 06:01:00 +0100

    Send profile id alongside with answers, add read me

---
+++ README.md
@@ -0,0 +1,19 @@
+## help me, help you
+
+### Table of Contents
+1. Why did we do that?
+2. How run project locally
+3. On-line demo
+4. Contributors
+
+
+### 1. Why did we do that?
+Project **help me, help you** have been created during [Junction 2020 Hackathon](https://connected.hackjunction.com/).
+The challenge states: "Create an innovative and implementable solution for drug use prevention among youth!"
+Our [demo video](https://www.youtube.com/watch?v=uZyKlZCjmMc&feature=youtu.be&ab_channel=Uplift) says more than thousand words.
+
+### 2. Hot run project locally
+Easiest way is to use docker-compose configuration. Prerequisites for that are docker and docker-compose installed. Then it's just as simple as firing invoking a command 'docker-compose up -d --build'.
+
+### 3. On-line demo
+App is available under http://www.help-you-help.me.
--- frontend/src/views/Questionnaire.vue
+++ frontend/src/views/Questionnaire.vue
@@ -3,87 +3,117 @@
     <app-bar :withoutButtons="true"></app-bar>
     <v-main class="grey lighten-5">
       <v-progress-linear :value="progress"></v-progress-linear>
-      <v-container style="height: 90%" class="d-flex justify-center align-center">
-        <div>
-          <question-card v-if="question" :question="question">
-          </question-card>
-          <v-btn class="float-left" :disabled="!previousAvailable" @click="previousClick">Previous</v-btn>
-          <v-btn class="primary float-right" :disabled="!nextAvailable" @click="nextClick">Next</v-btn>
-        </div>
+      <v-container
+        style="height: 90%"
+        class="d-flex justify-center align-center"
+      >
+        <transition name="fade">
+          <div>
+            <question-card v-if="question" :question="question">
+            </question-card>
+            <v-btn
+              class="float-left"
+              :disabled="!previousAvailable"
+              @click="previousClick"
+              >Previous</v-btn
+            >
+            <v-btn
+              class="primary float-right"
+              :disabled="!nextAvailable"
+              @click="nextClick"
+              >Next</v-btn
+            >
+          </div>
+        </transition>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 import QuestionCard from "@/components/QuestionCard";
 import AppBar from "@/components/AppBar";
 import questionsStore from "@/questionsStore";
 
 const axios = require("axios").default;
 
-
 export default {
-  name: 'Questionnaire',
+  name: "Questionnaire",
   components: {
     AppBar,
-    QuestionCard
+    QuestionCard,
   },
-  data: function () {
+  data: function() {
     return {
       questions: [],
       question: null,
-      progress: 0
-    }
+      progress: 0,
+    };
   },
 
   created() {
-    this.progress = this.$store.state.answeredQuestions.length * 10
-    this.nextQuestion()
+    this.progress = this.$store.state.answeredQuestions.length * 10;
+    this.nextQuestion();
   },
   methods: {
-    nextClick: function () {
-      this.nextQuestion()
+    nextClick: function() {
+      this.nextQuestion();
     },
-    previousClick: function () {
-      this.question = this.$store.state.answeredQuestions.pop()
-      console.log(this.question)
-      this.progress -= 10
+    previousClick: function() {
+      this.question = this.$store.state.answeredQuestions.pop();
+      console.log(this.question);
+      this.progress -= 10;
     },
-    nextQuestion: async function () {
+    nextQuestion: async function() {
       if (this.question != null) {
-        this.$store.commit('addQuestion', this.question)
+        this.$store.commit("addQuestion", this.question);
       }
-      let pastQuestions = this.$store.state.answeredQuestions.map(function (x) {
-        return {'question': x.pk, 'result': x.result}
-      })
-      let response = await axios.post('http://40.115.33.104:8000/questions/next-question', pastQuestions)
-      let question = response.data
-      if (question['last_question']) {
-        this.$router.push({name: 'matches'})
+      let pastQuestions = this.$store.state.answeredQuestions.map(function(x) {
+        return { question: x.pk, result: x.result };
+      });
+      let response = await axios.post(
+        "http://40.115.33.104:8000/questions/next-question",
+        pastQuestions
+      );
+      let question = response.data;
+      if (question["last_question"]) {
+        this.$router.push({ name: "matches" });
       }
-      this.progress = this.progress + 10
-      if (question.style === 'radio') {
-        question.result = null
-      } else if (question.style === 'multiple') {
-        question.result = []
-      } else if (question.style === 'slider') {
-        question.result = question['min_value']
+      this.progress = this.progress + 10;
+      if (question.style === "radio") {
+        question.result = null;
+      } else if (question.style === "multiple") {
+        question.result = [];
+      } else if (question.style === "slider") {
+        question.result = question["min_value"];
       }
-      this.question = question
-    }
+      this.question = question;
+    },
   },
   computed: {
-    previousAvailable: function () {
-      return this.$store.state.answeredQuestions.length > 0
+    previousAvailable: function() {
+      return this.$store.state.answeredQuestions.length > 0;
     },
-    nextAvailable: function () {
+    nextAvailable: function() {
       if (this.question == null || this.question.result == null) {
-        return false
+        return false;
       }
-      return !(this.question.style === 'multiple' && this.question.result.length <= 0);
-    }
-  }
-}
+      return !(
+        this.question.style === "multiple" && this.question.result.length <= 0
+      );
+    },
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
