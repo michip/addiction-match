1885f874bfa79ee29b93a65cfc1d6332c5bc2d0f
Piotr Proszowski <p.proszowski@gmail.com>
2020-11-08 06:27:58 +0100

    Pass profile id to matchmaking

--- frontend/src/views/Matches.vue
+++ frontend/src/views/Matches.vue
@@ -3,7 +3,10 @@
     <AppBar></AppBar>
     <v-content class="grey lighten-5">
       <v-container fill-height>
-        <MatchedPersonsList ref="matches" :matches="matches"></MatchedPersonsList>
+        <MatchedPersonsList
+          ref="matches"
+          :matches="matches"
+        ></MatchedPersonsList>
       </v-container>
     </v-content>
   </v-app>
@@ -14,11 +17,17 @@ import MatchedPersonsList from "../components/MatchedPersonsList";
 import AppBar from "../components/AppBar";
 const axios = require("axios").default;
 
+const isLoggedIn = this.$store.state.profileInfo !== null;
+var pk;
+if (isLoggedIn) {
+  pk = this.$store.state.profileInfo.pk;
+}
+
 export default {
   name: "Matches",
   components: { MatchedPersonsList, AppBar },
   mounted: function() {
-    console.log("matches: ", this.matches)
+    console.log("matches: ", this.matches);
     this.answers = this.$store.state.answeredQuestions.map(function(x) {
       return { question: x.pk, result: x.result };
     });
@@ -27,13 +36,13 @@ export default {
   data: function() {
     return {
       matches: [],
-      answers: Array
+      answers: Array,
     };
   },
   methods: {
     getMatchedPersons: function() {
       axios
-        .post("http://40.115.33.104:8000/questions/matchmake", this.answers)
+        .post("http://40.115.33.104:8000/questions/matchmake", {profile_id: isLoggedIn ? pk : undefined, answers: this.answers})
         .then((response) => {
           console.log(response.data);
           this.matches = response.data;
